98d08a05ef74d794f8b5c996db0b2c12150ae1c3
OuOHoon <wls2579@gmail.com>
2023-12-15 19:41:40 +0900

    8

--- 카테고리 별 상품 개수 구하기.sql
+++ 카테고리 별 상품 개수 구하기.sql
@@ -1,4 +1,5 @@
 -- 코드를 입력하세요
-SELECT count(*) 
-from user_info 
-where joined like '2021%' and age >= 20 and age <= 29
+SELECT substr(product_code, 1, 2) as category, count(*) as products 
+from product 
+group by category 
+order by category asc
