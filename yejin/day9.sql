fec30e1a94316f7939a35f7413d598ce010f41b8
leeyejin <leeyejin113@gmail.com>
2023-12-17 18:54:27 +0900

    day9

---
+++ yejin/day9.sql
@@ -0,0 +1,63 @@
+-- 코드를 입력하세요
+SELECT
+    FLAVOR
+FROM
+    FIRST_HALF
+ORDER BY
+    TOTAL_ORDER DESC,
+    SHIPMENT_ID
+
+
+-- 코드를 입력하세요
+SELECT
+    CAR_TYPE,
+    COUNT(*) AS CARS
+FROM
+    CAR_RENTAL_COMPANY_CAR
+WHERE
+    OPTIONS LIKE '%통풍시트%'
+   OR
+    OPTIONS LIKE '%열선시트%'
+   OR
+    OPTIONS LIKE '%가죽시트%'
+GROUP BY
+    CAR_TYPE
+ORDER BY
+    CAR_TYPE
+
+
+-- 코드를 입력하세요
+SELECT
+    CATEGORY,
+    SUM(SALES) AS TOTAL_SALES
+FROM
+    BOOK B,
+    BOOK_SALES BS
+WHERE
+    B.BOOK_ID = BS.BOOK_ID
+  AND
+    LEFT(BS.SALES_DATE,7) = '2022-01'
+GROUP BY
+    CATEGORY
+ORDER BY
+    CATEGORY
+
+
+-- 코드를 입력하세요
+SELECT
+    NAME,
+    DATETIME
+FROM
+    ANIMAL_INS AI
+WHERE
+    AI.ANIMAL_ID NOT IN
+    (
+        SELECT
+            ANIMAL_ID
+        FROM
+            ANIMAL_OUTS
+    )
+ORDER BY
+    DATETIME
+LIMIT
+    3
