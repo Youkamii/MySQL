0aadcaf7e9ba88a7b0962fe9419aa8e72b87da38
leeyejin <leeyejin113@gmail.com>
2023-12-20 11:28:30 +0900

    day11

---
+++ yejin/day11.sql
@@ -0,0 +1,61 @@
+-- 코드를 입력하세요
+SELECT
+    INGREDIENT_TYPE,
+    SUM(TOTAL_ORDER) AS TOTAL_ORDER
+FROM
+    ICECREAM_INFO i INNER JOIN
+    FIRST_HALF j
+    ON i.FLAVOR = j.FLAVOR
+GROUP BY
+    INGREDIENT_TYPE
+ORDER BY
+    TOTAL_ORDER
+
+
+-- 코드를 입력하세요
+SELECT
+    ANIMAL_ID,
+    NAME,
+    SEX_UPON_INTAKE
+FROM
+    ANIMAL_INS
+WHERE
+    NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY
+    ANIMAL_ID
+
+
+-- 코드를 입력하세요
+SELECT
+    ORDER_ID,
+    PRODUCT_ID,
+    DATE_FORMAT(OUT_DATE, '%Y-%m-%d') AS OUT_DATE,
+    CASE
+        WHEN OUT_DATE <= '2022-05-01' THEN '출고완료'
+        WHEN OUT_DATE > '2022-05-01' THEN '출고대기'
+        ELSE '출고미정'
+        END AS '출고여부'
+FROM
+    FOOD_ORDER
+ORDER BY
+    ORDER_ID
+
+
+-- 코드를 입력하세요
+SELECT
+    USER_ID,
+    NICKNAME,
+    SUM(PRICE) AS TOTAL_SALES
+FROM
+    USED_GOODS_BOARD b INNER JOIN
+    USED_GOODS_USER u
+    ON b.WRITER_ID = u.USER_ID
+WHERE
+    b.STATUS = 'DONE'
+GROUP BY
+    USER_ID
+HAVING
+    TOTAL_SALES >= 700000
+ORDER BY
+    TOTAL_SALES
+
