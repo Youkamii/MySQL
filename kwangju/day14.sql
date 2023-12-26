40b18c02e1c00c848ed161c1ef14389b72f9b85f
k <kkkjjj320320@naver.com>
2023-12-26 20:55:14 +0900

    14일차

---
+++ kwangju/day14.sql
@@ -0,0 +1,67 @@
+// 년, 월, 성별 별 상품 구매 회원 수 구하기
+
+SELECT
+    YEAR (SALES_DATE) AS YEAR, MONTH (SALES_DATE) AS MONTH, GENDER, COUNT (DISTINCT o.USER_ID) AS USERS
+FROM
+    ONLINE_SALE o JOIN
+    USER_INFO u
+ON o.USER_ID = u.USER_ID
+WHERE
+    GENDER IS NOT NULL
+GROUP BY
+    YEAR,
+    MONTH,
+    GENDER
+ORDER BY
+    YEAR,
+    MONTH,
+    GENDER
+
+
+// 서울에 위치한 식당 목록 출력하기
+
+SELECT ri.REST_ID,
+       REST_NAME,
+       FOOD_TYPE,
+       FAVORITES,
+       ADDRESS,
+       ROUND(AVG(REVIEW_SCORE), 2) AS SCORE
+FROM REST_INFO ri
+         JOIN
+     REST_REVIEW rr
+     ON ri.REST_ID = rr.REST_ID
+WHERE
+    LEFT (ADDRESS
+    , 2) = '서울'
+GROUP BY
+    ri.REST_ID
+ORDER BY
+    SCORE DESC,
+    FAVORITES DESC
+
+
+// 해비 유저가 소유한 장소
+
+SELECT *
+FROM PLACES
+WHERE HOST_ID IN
+      (SELECT HOST_ID
+       FROM PLACES
+       GROUP BY HOST_ID
+       HAVING COUNT(ID) >= 2)
+ORDER BY ID
+
+
+// 우유와 요거트가 담긴 장바구니
+
+SELECT DISTINCT CART_ID
+FROM CART_PRODUCTS
+WHERE CART_ID IN
+      (SELECT CART_ID
+       FROM CART_PRODUCTS
+       WHERE NAME = 'Milk')
+  AND CART_ID IN
+      (SELECT CART_ID
+       FROM CART_PRODUCTS
+       WHERE NAME = 'Yogurt')
+ORDER BY CART_ID
